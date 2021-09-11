976563c309db28389203d0a0062265f3087e0fcb
Wonhyo LEE <81315096+dldnjsgy5912@users.noreply.github.com>
2021-09-11 17:33:46 +0900

    Add files via upload

---
+++ project/main.js
@@ -0,0 +1,19 @@
+function returnFunc(){
+    return 1234;
+}
+returnFunc();
+
+const name1 = {
+    name:"dd",
+    age:58,
+    // 메소드
+    getName:function (){
+        return this.name;
+    }
+}
+
+console.log(name1.getName());
+
+const boxEl = document.querySelector('');
+
+boxEl.addEventListener(1,2)
